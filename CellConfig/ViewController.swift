81a92bc0e1eca5fbe1b195aaf011aacd392028a9
Abdoelrahman Eaita <dhooom.da@gmail.com>
2020-09-15 19:07:16 +0200

    Initial Commit

---
+++ CellConfig/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CellConfig
+//
+//  Created by Abdoelrhman Eaita on 9/15/20.
+//  Copyright © 2020 abdeenlabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
